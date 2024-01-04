92543eebf5ab9530b622619e202fcb09cbdac0a6
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-01-04 19:10:12 +0530

    Implemented countLetters method

--- CaesarBreaker.java
+++ CaesarBreaker.java
@@ -1,4 +1,4 @@
-
+import edu.duke.*;
 /**
  * Write a description of CaesarBreaker here.
  * 
@@ -6,6 +6,26 @@
  * @version (a version number or a date)
  */
 public class CaesarBreaker {
+    private int[] countLetters(FileResource resource) {
+        String message = resource.asString();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int[] counts = new int[26];
+        for (int i = 0; i < message.length(); i++) {
+            char ch = Character.toLowerCase(message.charAt(i));
+            int index = alphabet.indexOf(ch);
+            if(index != -1) {
+                counts[index]++;
+            }
+        }
+        return counts;
+    }
     
+    public void testCountLetters() {
+        FileResource resource = new FileResource();
+        int[] counts = countLetters(resource);
+        for (int i : counts) {
+            System.out.println(i);
+        }
+    }
 
 }
