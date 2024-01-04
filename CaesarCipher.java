5ff25d84ad93dbd62ceb36aadccd0ca4e9a58dec
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-01-04 18:20:17 +0530

    Implemented encryptTwoKeys method

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -29,6 +29,34 @@ public class CaesarCipher {
         return encrypted.toString();
     }
     
+    public String encryptTwoKeys(String input, int key1, int key2) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder encrypted = new StringBuilder(input);
+        String encryptedAlphabetKey1 = alphabet.substring(key1) + alphabet.substring(0, key1);
+        String encryptedAlphabetKey2 = alphabet.substring(key2) + alphabet.substring(0, key2);
+        for(int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            int idx = alphabet.indexOf(Character.toUpperCase(currChar));
+            if (idx != -1) {
+                char newChar;
+                if (i % 2 == 0) {
+                    newChar = encryptedAlphabetKey1.charAt(idx);
+                }
+                else {
+                    newChar = encryptedAlphabetKey2.charAt(idx);
+                }
+                if(Character.isUpperCase(encrypted.charAt(i))) {
+                    newChar = Character.toUpperCase(newChar);
+                }
+                else {
+                    newChar = Character.toLowerCase(newChar);
+                }
+                encrypted.setCharAt(i, newChar);
+            }
+        }
+        return encrypted.toString();
+    }
+    
     public void testEncrypt() {
         //System.out.println( encrypt("FIRST LEGION ATTACK EAST FLANK!", 23));
         //System.out.println( encrypt("First Legion", 23));
