54ba8420720210ae98100f2075ce5fa7eb1fce3d
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-01-04 18:13:16 +0530

    Implemented encrypt method

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -6,6 +6,26 @@
  * @version (a version number or a date)
  */
 public class CaesarCipher {
+    public String encrypt(String input, int key) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder encrypted = new StringBuilder(input);
+        String encryptedAlphabet = alphabet.substring(key) + alphabet.substring(0, key);
+        for(int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            int idx = alphabet.indexOf(Character.toUpperCase(currChar));
+            if (idx != -1) {
+                char newChar = encryptedAlphabet.charAt(idx);
+                if(Character.isUpperCase(encrypted.charAt(i))) {
+                    newChar = Character.toUpperCase(newChar);
+                }
+                else {
+                    newChar = Character.toLowerCase(newChar);
+                }
+                encrypted.setCharAt(i, newChar);
+            }
+        }
+        return encrypted.toString();
+    }
     
     public static void main(String[] args) {
         
