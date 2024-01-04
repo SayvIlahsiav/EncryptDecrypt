6c8d40ca17fce918009e34caf152925f9dd7a4fa
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-01-04 18:19:40 +0530

    Implemented testEncrypt method

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,3 +1,5 @@
+import edu.duke.*;
+import java.util.*;
 
 /**
  * Write a description of CaesarCipher here.
@@ -25,6 +27,20 @@ public class CaesarCipher {
             }
         }
         return encrypted.toString();
+    }
+    
+    public void testEncrypt() {
+        //System.out.println( encrypt("FIRST LEGION ATTACK EAST FLANK!", 23));
+        //System.out.println( encrypt("First Legion", 23));
+        //System.out.println( encrypt("First Legion", 17));
+        //System.out.println( encrypt("At noon be in the conference room with your hat on for a surprise party. YELL LOUD!", 15));
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter key to encrypt message: ");
+        int key = input.nextInt();
+        String encrypted = encrypt(message, key);
+        System.out.println("\nEncrypted Message:\n" + encrypted);
     }
     
     public static void main(String[] args) {
